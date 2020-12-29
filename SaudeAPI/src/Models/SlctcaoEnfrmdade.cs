baec7a1a6af58495cf268fd5c93e1dfd6c5873e6
Isabelle Lima <isabelle.lima@delage.com.br>
2020-12-28 22:11:17 -0300

    Correcao

--- SaudeAPI/src/Models/SlctcaoEnfrmdade.cs
+++ SaudeAPI/src/Models/SlctcaoEnfrmdade.cs
@@ -3,6 +3,7 @@ namespace SaudeAPI.Models
     public class SlctcaoEnfrmdade
     {
         [Key]
+        //Comentario
         public int CdSlctcaoEnfrmdade {get; set;}
         public int CdSlctcao {get; set;}
         public int CdEnfrmdade {get;set;}
